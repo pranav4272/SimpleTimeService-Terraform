6d705aa2aefa5c46641d71128b62e7c27e3b1c5c
pranav4272 <101634223+pranav4272@users.noreply.github.com>
2025-04-14 22:18:51 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
 // main.tf - Terraform main config
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
