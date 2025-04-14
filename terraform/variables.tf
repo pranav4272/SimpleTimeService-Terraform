1c62a148dba664c3708451d61c650125117635cc
pranav4272 <101634223+pranav4272@users.noreply.github.com>
2025-04-14 22:19:14 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,14 @@
 // variables.tf terraform variable config
+
+variable "resource_group_name" {
+  default = "simpletimeservice-rg"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "prefix" {
+  default = "sts"
+}
+
